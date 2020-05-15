dc0c76b4bddb8df9ef7633cbb74d9bccd9db4233
Twixes <dev@twixes.com>
2020-05-15 21:22:39 +0200

    Improve interface naming

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode'
 import fetch, { Response } from 'node-fetch'
 
-interface PackageSpec {
+interface PackageRequirement {
     id: string
     id_raw: string
     extras: string[]
@@ -28,7 +28,7 @@ function linkify(text: string, link?: string): string {
     return link ? `[${text}](${link})`: text
 }
 
-function extractPackageSpec(line: vscode.TextLine): PackageSpec | null {
+function extractPackageRequirement(line: vscode.TextLine): PackageRequirement | null {
     const match = line.text.replace(/\s/g, '').match(deconstructionRe)
     if (match === null) return null
     let constraints: [string, string][] = []
@@ -42,9 +42,9 @@ function extractPackageSpec(line: vscode.TextLine): PackageSpec | null {
     }
 }
 
-async function fetchPackageInfo(spec: PackageSpec): Promise<[number | null, PackageInfo | null]> {
+async function fetchPackageInfo(requirement: PackageRequirement): Promise<[number | null, PackageInfo | null]> {
     try {
-        const response: Response = await fetch(`https://pypi.org/pypi/${spec.id}/json`)
+        const response: Response = await fetch(`https://pypi.org/pypi/${requirement.id}/json`)
         let info: PackageInfo | null = null
         if (response.ok) info = (await response.json()).info
         return [response.status, info]
@@ -67,19 +67,20 @@ function presentPackageInfo(info: PackageInfo): string {
 }
 
 async function provideHover(document: vscode.TextDocument, position: vscode.Position) {
-    const spec: PackageSpec | null = extractPackageSpec(document.lineAt(position.line))
-    if (spec === null) return new vscode.Hover('')
-    let infoPresentation: string | undefined = infoPresentationCache.get(spec.id)
+    const requirement: PackageRequirement | null = extractPackageRequirement(document.lineAt(position.line))
+    if (requirement === null) return new vscode.Hover('')
+    let infoPresentation: string | undefined = infoPresentationCache.get(requirement.id)
     if (infoPresentation === undefined) {
-        const [status, info]: [number | null, PackageInfo | null] = await fetchPackageInfo(spec)
+        const [status, info]: [number | null, PackageInfo | null] = await fetchPackageInfo(requirement)
         if (status === 200) infoPresentation = presentPackageInfo(info!)
         else if (status === 404) infoPresentation = `{id_raw} is not available in PyPI`
-        if (infoPresentation) infoPresentationCache.set(spec.id, infoPresentation)
+        if (infoPresentation) infoPresentationCache.set(requirement.id, infoPresentation)
         else infoPresentation = linkify(
-            `could not fetch ${spec.id_raw} information from PyPI`, `https://pypi.org/project/${spec.id_raw}/`
+            `could not fetch ${requirement.id_raw} information from PyPI`,
+            `https://pypi.org/project/${requirement.id_raw}/`
         )
     }
-    return new vscode.Hover(infoPresentation.replace('{id_raw}', spec.id_raw))
+    return new vscode.Hover(infoPresentation.replace('{id_raw}', requirement.id_raw))
 }
 
 export function activate(_: vscode.ExtensionContext) {
